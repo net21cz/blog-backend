70619ff7f9d100a81786b66373403c56126b4781
Tomas Tulka <tomas.tulka@gmail.com>
2018-09-20 20:19:10 +0200

    Access-Control-Allow added

--- articles.php
+++ articles.php
@@ -3,7 +3,8 @@ namespace blog\articles;
 
 use db;
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://blog.net21.cz");
+header("Access-Control-Allow-Methods: GET");
 header("Content-Type: application/json; charset=UTF-8");
 
 require_once __DIR__ . '/config/db.config.php';
